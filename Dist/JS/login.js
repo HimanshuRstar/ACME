c527d959d96b110745ade385140852dbaaf8b3b2
Himanshu Kathuria <himanshu.kathuria@rstartec.com>
2022-04-18 17:14:34 +0530

    Fixed validation and Quotes page

--- Dist/JS/login.js
+++ Dist/JS/login.js
@@ -1,19 +1,19 @@
-function validateForm() {
-    let email = document.getElementById("email").value;
-    let pass = document.getElementById("password").value;
-    let email1 = "himanshukathuria1997@gmail.com"
-    let pass1 = "Himanshu123"
+// function validateForm() {
+//     let email = document.getElementsByClassName("email_validate").value;
+//     let pass = document.getElementsByClassName("class_validate").value;
+//     let email1 = "himanshukathuria1997@gmail.com"
+//     let pass1 = "Himanshu123"
 
-    if (email == null || email != email1) {
-        alert("Email is not correct");
-        return false;
-    }
-    else if (pass.length < 6 || pass != pass1) {
+//     if (email == null || email != email1) {
+//         alert("Email is not correct");
+//         return false;
+//     }
+//     else if (pass.length < 6 || pass != pass1) {
 
-        alert("Password is not correct");
-        return false;
-    }
-}
+//         alert("Password is not correct");
+//         return false;
+//     }
+// }
 
 // function validateForm() {
 //     let email = document.getElementById("email").value;
