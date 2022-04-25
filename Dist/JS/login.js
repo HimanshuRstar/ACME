cad2a42fb4bd9f605078a50872e393371d90f448
Himanshu Kathuria <himanshu.kathuria@rstartec.com>
2022-04-25 11:13:13 +0530

    login page

--- Dist/JS/login.js
+++ Dist/JS/login.js
@@ -1,4 +1,4 @@
-let registeredUsers = [
+let users = [
     {
         "email": "sonu@gmail.com",
         "password": "sonu^%$ert"
@@ -23,41 +23,126 @@ let registeredUsers = [
 ]
 
 
-let emailVal = document.getElementById("email_validate").value;
-let pwdVal = document.getElementById("pass_validate").value;
-let error = document.getElementById("validationError");
+document.getElementById('loginBtn').onclick = function () {
+    var emailVal = document.getElementById('email_validate').value
+    var pwdVal = document.getElementById('pass_validate').value
+    let error = document.getElementById("validationError");
+    for (var i = 0; i < users.length; i++) {
+        if (emailVal == users[i].email && emailVal != null) {
+            if (pwdVal == users[i].password && pwdVal != null) {
+                error.innerHTML = 'Success!'
+                return;
+            } else {
+                error.innerHTML = 'Please fill the Correct Password'
+                return false;
+            }
+        }
 
-
-function checkValidation() {
-    let object = registeredUsers.find(obj => obj.email == emailVal && obj.password == pwdVal);
-    if (object != null)
-        console.log('Login successfull!');
-    else {
-        let findObject = registeredUsers.find(obj => obj.email == emailVal);
-        if (findObject != null)
-            pwdValidate();
-        else
-            emailValidate();
     }
+    error.innerHTML = 'Please fill the Correct Email'
+    return false;
+
 }
 
-function emailValidate() {
-    let object = registeredUsers.find(obj => obj.email == emailVal);
-    if (object) {
-        error.innerHTML = "Email Address is Valid."
-    } else if (emailVal == null) {
-        error.innerHTML = "Please fill the email."
-    }
-}
 
-function pwdValidate() {
-    let object = registeredUsers.find(obj => obj.email == emailVal);
-    if (object) {
-        error.innerHTML = "Email Address is Valid."
-    } else if (emailVal == null) {
-        error.innerHTML = "Please fill the email."
-    }
-}
+
+// function validateForm() {
+//     let txtEmailVal = document.getElementById("email_validate").value;
+//     let txtPwdVal = document.getElementById("pass_validate").value;
+//     // let error = document.getElementById("validationError")
+
+//     let registeredUsers = [
+//         {
+//             "email": "sonu@gmail.com",
+//             "password": "sonu^%$ert"
+//         },
+//         {
+//             "email": "himanshu@gmail.com",
+//             "password": "himanshu^%$ert"
+//         },
+//         {
+//             "email": "aashna@gmail.com",
+//             "password": "aashna^%$ert"
+//         },
+//         {
+//             "email": "vishnu@gmail.com",
+//             "password": "vishnu^%$ert"
+//         },
+
+//         {
+//             "email": "umesh@gmail.com",
+//             "password": "umesh^%$ert"
+//         }
+//     ]
+
+//     let object = registeredUsers.find(obj => obj.email == txtEmailVal && obj.password == txtPwdVal);
+//     console.log('testobject', object);
+//     if (object == null) {
+//         alert('Pls validate your form')
+//         // error.innerHTML = "Email Address is Not Valid."
+//         return false;
+//     }
+//     else if (txtPwdVal.length < 6) {
+//         alert('testing password')
+//         return false;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function checkValidation() {
+//     let emailVal = document.getElementById("email_validate").value;
+//     let pwdVal = document.getElementById("pass_validate").value;
+//     // let error = document.getElementById("validationError");
+//     let object = registeredUsers.find(obj => obj.email == emailVal && obj.password == pwdVal);
+//     if (object != null)
+//         console.log('Login successfull!');
+//     else {
+//         let findObject = registeredUsers.find(obj => obj.email == emailVal);
+//         if (findObject != null)
+//             pwdValidate();
+//         else
+//             emailValidate();
+//     }
+// }
+
+// function emailValidate() {
+
+//     let error = document.getElementById("validationError");
+//     let object = registeredUsers.find(obj => obj.email == emailVal);
+//     if (object) {
+//         error.innerHTML = "Email Address is Valid."
+//     } else if (emailVal == null) {
+//         error.innerHTML = "Please fill the email."
+//     }
+// }
+
+// function pwdValidate() {
+
+//     let error = document.getElementById("validationError");
+//     let object = registeredUsers.find(obj => obj.password == pwdVal);
+//     if (object) {
+//         error.innerHTML = "Email Address is Valid."
+//     } else if (emailVal == null) {
+//         error.innerHTML = "Please fill the email."
+//     }
+// }
 
 
 
